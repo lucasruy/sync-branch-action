4506f8789e43e57c259bed9b056caad9230395e8
Lucas Ruy <lukas.r@hotmail.com>
2021-05-30 14:51:31 -0300

    chore: add import to the setVariablesValues to improve code

--- src/action.js
+++ src/action.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+const setVariablesValues = require('./set-variables-values')
 const getPullsListByBranch = require('./get-pulls-list')
 const createNewPullRequest = require('./create-new-pull')
 
@@ -8,8 +9,6 @@ async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
   const SOURCE_BRANCH = core.getInput('SOURCE_BRANCH')
   const DESTINATION_BRANCH = core.getInput('DESTINATION_BRANCH')
-  const PULL_REQUEST_TITLE = core.getInput('PULL_REQUEST_TITLE')
-  const PULL_REQUEST_BODY = core.getInput('PULL_REQUEST_BODY')
 
   const octokit = github.getOctokit(GITHUB_TOKEN)
   const { repository } = github.context.payload
@@ -17,38 +16,20 @@ async function run() {
   core.info('Starting process to make a new pull request...')
 
   try {
-    core.info('Verifying if required fields is setted.')
-    if (!SOURCE_BRANCH || !DESTINATION_BRANCH) {
-      throw new Error('You need to enter a valid value in the "SOURCE_BRANCH" and "DESTINATION_BRANCH" fields.')
-    }
-
-    const repo = repository.name
-    const owner = repository.owner.login
-
-    const title = PULL_REQUEST_TITLE || `
-      update: ${DESTINATION_BRANCH} to ${SOURCE_BRANCH}
-    `
-    const body = PULL_REQUEST_BODY || `
-      This is an automatic Pull Request to keep ${DESTINATION_BRANCH} up to date with ${SOURCE_BRANCH}! 🔄
-    `
+    const { repositoryValues, pullRequestValues } = setVariablesValues(repository)
 
     const openPullRequest = await getPullsListByBranch(octokit, {
-      owner,
-      repo,
+      ...repositoryValues,
       head: SOURCE_BRANCH,
     })
 
     if (!openPullRequest) {
-      const params = {
-        owner,
-        repo,
-        body,
-        title,
+      await createNewPullRequest(octokit, {
+        ...repositoryValues,
+        ...pullRequestValues,
         head: SOURCE_BRANCH,
         base: DESTINATION_BRANCH,
-      }
-
-      await createNewPullRequest(octokit, params)
+      })
       return
     }
 
