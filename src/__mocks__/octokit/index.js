34b3fd6c1b5f884b18dc0b94ca547e16b80bd08b
Sebastian Krzyżanowski <eskrzy@gmail.com>
2022-11-22 10:18:22 +0100

    feat: add label support

--- src/__mocks__/octokit/index.js
+++ src/__mocks__/octokit/index.js
@@ -1,27 +1,51 @@
 const createResponse = () => Promise.resolve({
     data: {
-        number: 134,
-        url: 'https://api.github.com/repos/octocat/Hello-World/pulls/134',
-        title: "Amazing new feature",
+        number: 1,
+        url: 'https://api.github.com/repos/sbsrnt/Hello-World/pulls/1',
+        title: "Update README",
+        labels: ['ci'],
     }
 })
 
 const listResponse = () => Promise.resolve({
     data: [
         {
-            number: 134,
-            url: 'https://api.github.com/repos/octocat/Hello-World/pulls/134',
-            title: "Amazing new feature",
+            number: 1,
+            url: 'https://api.github.com/repos/sbsrnt/Hello-World/pulls/1',
+            title: "Update README",
+            labels: ['ci'],
             state: "open"
         },
         {
-            number: 135,
-            url: 'https://api.github.com/repos/octocat/Hello-World/pulls/135',
-            title: "Amazing new feature",
+            number: 2,
+            url: 'https://api.github.com/repos/sbsrnt/Hello-World/pulls/2',
+            title: "Update README",
+            labels: ['ci'],
             state: "close"
         }
     ]
 })
+
+const createLabelResponse = () => Promise.resolve({
+    data: {
+        name: "ci"
+    }
+})
+
+const getLabelResponse = () => Promise.resolve({
+    data: {
+        name: "ci"
+    }
+})
+
+const addLabelsToPrResponse = () => Promise.resolve({
+    data: [
+        {
+            name: 'ci'
+        }
+    ]
+})
+
 
 const mockedValue = () => {
     return {
@@ -29,6 +53,11 @@ const mockedValue = () => {
             pulls: {
                 create: jest.fn().mockImplementation(createResponse),
                 list: jest.fn().mockImplementation(listResponse)
+            },
+            issues: {
+                getLabel: jest.fn().mockImplementation(getLabelResponse),
+                createLabel: jest.fn().mockImplementation(createLabelResponse),
+                addLabels: jest.fn().mockImplementation(addLabelsToPrResponse)
             }
         }
     }
